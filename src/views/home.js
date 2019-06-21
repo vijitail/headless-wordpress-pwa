295df841f0be0583278ca98d0c5389f693a02d7c
vijitail <vijitail2@gmail.com>
2019-06-21 13:20:38 +0530

    Displaying API data in views

--- src/views/home.js
+++ src/views/home.js
@@ -1,8 +1,13 @@
-export default (props, data) => {
+export default (props, posts) => {
+    const renderDate = dateString => {
+        const date = new Date(dateString);
+        return date.toLocaleDateString();
+    }
+
     return(/*html*/`
     <div class="container">
         <div class="row">
-            ${(data.loading === true) ? [1,2,3].map(() => /*html*/`
+            ${(posts.loading === true) ? [1,2,3].map(() => /*html*/`
             <div class="col">
                 <div class="card" data-loading="true">
                     <img class="post-thumbnail" src="https://via.placeholder.com/100x45.png?text=Loading">
@@ -13,41 +18,19 @@ export default (props, data) => {
                     </article>
                 </div>
             </div>
-            `) : /*html*/`
+            `) : posts.map(post =>/*html*/`
             <div class="col">
                 <div class="card">
-                    <img class="post-thumbnail" src="https://www.elegantthemes.com/blog/wp-content/uploads/2019/06/biteable-review-featured-image.jpg">
+                    <img class="post-thumbnail" src=${post.jetpack_featured_media_url} />
                     <article>
-                        <h4 class="post-title">Lorem Ipsum - Hello World</h4>
-                        <p class="post-meta">23 December</p>
-                        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+                        <h4 class="post-title">${post.title.rendered}</h4>
+                        <p class="post-meta">${renderDate(post.date)}</p>
+                        ${post.excerpt.rendered}
                         <a class="btn" href="#">Read Post</a>
                     </article>
                 </div>
             </div>
-            <div class="col">
-                <div class="card">
-                    <img class="post-thumbnail" src="https://www.elegantthemes.com/blog/wp-content/uploads/2019/06/biteable-review-featured-image.jpg">
-                    <article>
-                        <h4 class="post-title">Lorem Ipsum - Hello World</h4>
-                        <p class="post-meta">23 December</p>
-                        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
-                        <a class="btn" href="#">Read Post</a>
-                    </article>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card">
-                    <img class="post-thumbnail" src="https://www.elegantthemes.com/blog/wp-content/uploads/2019/06/biteable-review-featured-image.jpg">
-                    <article>
-                        <h4 class="post-title">Lorem Ipsum - Hello World</h4>
-                        <p class="post-meta">23 December</p>
-                        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
-                        <a class="btn" href="#">Read Post</a>
-                    </article>
-                </div>
-            </div>
-            `}
+            `)}
             
         </div>
     </div>
